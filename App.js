9bbdae2789c9e2bfc16a914dfdbbca275e4a271c
Mia <55879125+mijaka@users.noreply.github.com>
2021-02-11 11:19:19 +0200

    app

---
+++ App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {isHoverEnabled} from "react-native/Libraries/Pressability/HoverState";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <View style={styles.carContainer}>
+
+    <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}> Starting at $68000</Text>
+    </View>
+
+    </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+    carContainer:{
+    width: '100%',
+        height: '100%',
+    },
+        titles:{
+          marginTop:'30%',
+            width: '100%',
+            alignItems: 'center',
+        },
+    title:{
+       fontSize: 40,
+        fontWeight: '500',
+    },
+    subtitle:{
+       fontSize: 16,
+        color:'#5c5e62',
+    }
+
+
+});
